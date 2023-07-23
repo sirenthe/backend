a96ee86467fff9745e762428c179d62287538dee
sirenthe <lamanrj@code.edu.az>
2023-07-23 15:15:26 +0400

    hdhh

--- P330Pronia/Controllers/HomeController.cs
+++ P330Pronia/Controllers/HomeController.cs
@@ -1,22 +1,62 @@
-﻿using System.Runtime.CompilerServices;
+﻿//using System.Runtime.CompilerServices;
+//using Microsoft.AspNetCore.Mvc;
+//using P330Pronia.Contexts;
+
+//namespace P330Pronia.Controllers
+//{
+//	public class HomeController : Controller
+//	{
+//		private readonly AppDbContext _context;
+//		public HomeController(AppDbContext context)
+//		{
+//			_context = context;
+//		}
+//		public IActionResult Index()
+
+
+//		{
+//			var sliders = _context.Sliders;
+//			return View(sliders);
+
+//		}
+
+//		public IActionResult Index2()
+
+
+//		{
+//			var cards = _context.cards;
+//			return View(cards);
+//		}
+//	}
+//}
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using P330Pronia.Contexts;
+using P330Pronia.Models;
 
 namespace P330Pronia.Controllers
 {
 	public class HomeController : Controller
-	{ 
+	{
 		private readonly AppDbContext _context;
+
 		public HomeController(AppDbContext context)
 		{
 			_context = context;
 		}
-		public IActionResult Index()
 
-			
+		public IActionResult Index()
 		{
-			var sliders = _context.Sliders;
-			return View(sliders);
+			var viewModel = new MyViewModel
+			{
+				Sliders = _context.Sliders,
+				Cards = _context.cards
+			};
+
+			return View(viewModel);
 		}
 	}
 }
+
+
+
--- P330Pronia/Models/MyViewModel.cs
+++ P330Pronia/Models/MyViewModel.cs
@@ -1,6 +1,10 @@
 ﻿namespace P330Pronia.Models
+	
 {
 	public class MyViewModel
 	{
+		public IEnumerable<Slider> Sliders { get; set; }
+		public IEnumerable<cards> Cards { get; set; }
 	}
+
 }
